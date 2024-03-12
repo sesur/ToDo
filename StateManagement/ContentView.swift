36bf5cf566b2bf07cbfbe785fd286c3b18253664
sesur <sergiusuru@ymail.com>
2024-03-12 09:23:43 +0200

    Initial Commit

---
+++ StateManagement/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StateManagement
+//
+//  Created by Sergiu on 12.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StateManagement/StateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateManagementApp.swift
+//  StateManagement
+//
+//  Created by Sergiu on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct StateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
